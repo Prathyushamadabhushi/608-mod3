aaab393d2819e93ddf28ebeea9d7493ced6ad6e7
Prathyusha <prathyusham0001@gmail.com>
2021-09-07 03:17:15 -0400

    Update bult-in-min-max.py

--- bult-in-min-max.py
+++ bult-in-min-max.py
@@ -6,5 +6,5 @@ def maximum(value1, value2, value3):
     if value3 > max_value:
         max_value = value3
     return max_value
-print("Maximum number: ", maximum(12, 27, 36))
+print("Maximum number: ", max(12, 27, 36))
 print("Minimum number: ",min(15, 9, 27, 14))
