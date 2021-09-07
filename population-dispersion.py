3ed0e04791c2d95f66215b2841415a5ad597f0e1
Prathyusha <prathyusham0001@gmail.com>
2021-09-07 03:14:33 -0400

    Create population-dispersion.py

---
+++ population-dispersion.py
@@ -0,0 +1,3 @@
+import statistics
+print("Get Variance: ", statistics.pvariance([1, 3, 4, 2, 6, 5, 3, 4, 5, 2]))
+print("Get Standard Deviation: ", statistics.pstdev([1, 3, 4, 2, 6, 5, 3, 4, 5, 2]))
